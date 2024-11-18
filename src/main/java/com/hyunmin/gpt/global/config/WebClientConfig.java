ce3fa85a5e28011dee329cdc8f0a10af9246b2e8
hyunmin0317 <hyunmin-choi@naver.com>
2024-11-18 21:15:38 +0900

    chore: ChatGPT API WebClient 설정 (#1)

---
+++ src/main/java/com/hyunmin/gpt/global/config/WebClientConfig.java
@@ -0,0 +1,27 @@
+package com.hyunmin.gpt.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${openai.api.url}")
+    private String apiUrl;
+
+    @Value("${openai.api.key}")
+    private String apiKey;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(apiUrl)
+                .defaultHeader("Authorization", "Bearer " + apiKey)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
