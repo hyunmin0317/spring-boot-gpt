22c6d3a7fb8e4ed022e88a415c377ece85c77c2c
hyunmin0317 <hyunmin-choi@naver.com>
2024-11-27 20:45:25 +0900

    fix: ObjectMapper 빈 등록으로 인한 오류 (#14)

--- src/main/java/com/hyunmin/gpt/global/config/WebClientConfig.java
+++ src/main/java/com/hyunmin/gpt/global/config/WebClientConfig.java
@@ -1,6 +1,5 @@
 package com.hyunmin.gpt.global.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,10 +23,5 @@ public class WebClientConfig {
                 .defaultHeader("Authorization", "Bearer " + apiKey)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
     }
 }
