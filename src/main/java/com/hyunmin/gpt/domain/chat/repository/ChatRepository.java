3ddfd99fb414a7fd3dccdb06651ef740698a84c9
hyunmin0317 <hyunmin-choi@naver.com>
2024-11-27 20:33:49 +0900

    feat: Member FK 저장 (#14)

--- src/main/java/com/hyunmin/gpt/domain/chat/repository/ChatRepository.java
+++ src/main/java/com/hyunmin/gpt/domain/chat/repository/ChatRepository.java
@@ -3,6 +3,9 @@ package com.hyunmin.gpt.domain.chat.repository;
 import com.hyunmin.gpt.domain.chat.entity.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChatRepository extends JpaRepository<Chat, String> {
 
+    Optional<Chat> findByIdAndMemberId(String id, Long member_id);
 }
