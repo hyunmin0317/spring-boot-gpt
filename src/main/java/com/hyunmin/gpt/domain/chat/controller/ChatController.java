315951f9cd5f22beea80e0e21465aa6280a65922
hyunmin0317 <hyunmin-choi@naver.com>
2024-11-30 12:30:07 +0900

    refactor: 채팅 서비스 CQRS 패턴 적용 (#22)

--- src/main/java/com/hyunmin/gpt/domain/chat/controller/ChatController.java
+++ src/main/java/com/hyunmin/gpt/domain/chat/controller/ChatController.java
@@ -3,8 +3,9 @@ package com.hyunmin.gpt.domain.chat.controller;
 import com.hyunmin.gpt.domain.chat.dto.ChatGptRequestDto;
 import com.hyunmin.gpt.domain.chat.dto.ChatRequestDto;
 import com.hyunmin.gpt.domain.chat.dto.ChatResponseDto;
+import com.hyunmin.gpt.domain.chat.service.ChatCommandService;
 import com.hyunmin.gpt.domain.chat.service.ChatGptService;
-import com.hyunmin.gpt.domain.chat.service.ChatService;
+import com.hyunmin.gpt.domain.chat.service.ChatQueryService;
 import com.hyunmin.gpt.domain.message.service.MessageService;
 import com.hyunmin.gpt.global.security.annotation.AuthMember;
 import jakarta.validation.Valid;
@@ -22,13 +23,14 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/api/v1/chat")
 public class ChatController {
 
-    private final ChatService chatService;
+    private final ChatQueryService chatQueryService;
+    private final ChatCommandService chatCommandService;
     private final ChatGptService chatGptService;
     private final MessageService messageService;
 
     @PostMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<Flux<String>> streamChat(@AuthMember Long memberId, @RequestBody @Valid ChatRequestDto requestDto) {
-        String chatId = chatService.getOrCreateChatId(memberId, requestDto);
+        String chatId = chatCommandService.getOrCreateChatId(memberId, requestDto);
         ChatGptRequestDto gptRequestDto = messageService.readMessagesForChatGpt(chatId, requestDto.content());
         Flux<String> responseFlux = chatGptService.streamChat(chatId, gptRequestDto, requestDto.content());
         return ResponseEntity.ok()
@@ -38,7 +40,7 @@ public class ChatController {
 
     @GetMapping
     public ResponseEntity<Slice<ChatResponseDto>> readChats(@AuthMember Long memberId, @ParameterObject Pageable pageable) {
-        Slice<ChatResponseDto> responseDtoSlice = chatService.readChats(memberId, pageable);
+        Slice<ChatResponseDto> responseDtoSlice = chatQueryService.readChats(memberId, pageable);
         return ResponseEntity.ok(responseDtoSlice);
     }
 }
--- src/main/java/com/hyunmin/gpt/domain/chat/service/ChatQueryService.java
+++ src/main/java/com/hyunmin/gpt/domain/chat/service/ChatQueryService.java
@@ -1,11 +1,8 @@
 package com.hyunmin.gpt.domain.chat.service;
 
-import com.hyunmin.gpt.domain.chat.dto.ChatRequestDto;
 import com.hyunmin.gpt.domain.chat.dto.ChatResponseDto;
 import com.hyunmin.gpt.domain.chat.entity.Chat;
 import com.hyunmin.gpt.domain.chat.repository.ChatRepository;
-import com.hyunmin.gpt.global.common.entity.Member;
-import com.hyunmin.gpt.global.common.repository.MemberRepository;
 import com.hyunmin.gpt.global.exception.GeneralException;
 import com.hyunmin.gpt.global.exception.code.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class ChatService {
+public class ChatQueryService {
 
     private final ChatRepository chatRepository;
-    private final MemberRepository memberRepository;
 
     public Slice<ChatResponseDto> readChats(Long memberId, Pageable pageable) {
         Slice<Chat> chatSlice = chatRepository.findAllChats(memberId, pageable);
@@ -31,22 +27,5 @@ public class ChatService {
         Chat chat = chatRepository.findByIdAndMemberId(chatId, memberId)
                 .orElseThrow(() -> new GeneralException(ErrorCode.CHAT_NOT_FOUND));
         return ChatResponseDto.from(chat);
-    }
-
-    @Transactional
-    public ChatResponseDto createChat(Long memberId, ChatRequestDto requestDto) {
-        Chat chat = requestDto.toEntity();
-        if (memberId != null) {
-            Member member = memberRepository.findById(memberId)
-                    .orElseThrow(() -> new GeneralException(ErrorCode.ACCOUNT_NOT_FOUND));
-            chat.setMember(member);
-        }
-        return ChatResponseDto.from(chatRepository.save(chat));
-    }
-
-    @Transactional
-    public String getOrCreateChatId(Long memberId, ChatRequestDto requestDto) {
-        return requestDto.chatId() != null ?
-                readChat(memberId, requestDto.chatId()).id() : createChat(memberId, requestDto).id();
     }
 }
