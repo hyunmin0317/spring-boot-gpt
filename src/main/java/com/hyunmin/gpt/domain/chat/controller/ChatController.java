b8641550fe64ca7f2750db2afdd7a83e5506f486
Hyunmin Choi <hyunmin-choi@naver.com>
2024-11-30 10:41:54 +0900

    [FEATURE] GPT API 멀티턴 적용 (#21)
    * feat: GPT API 멀티턴 적용 (#18)
    
    * refactor: dto 리팩토링 (#18)
    
    * refactor: 메서드명 리팩토링 (#18)

--- src/main/java/com/hyunmin/gpt/domain/chat/controller/ChatController.java
+++ src/main/java/com/hyunmin/gpt/domain/chat/controller/ChatController.java
@@ -1,9 +1,11 @@
 package com.hyunmin.gpt.domain.chat.controller;
 
+import com.hyunmin.gpt.domain.chat.dto.ChatGptRequestDto;
 import com.hyunmin.gpt.domain.chat.dto.ChatRequestDto;
 import com.hyunmin.gpt.domain.chat.dto.ChatResponseDto;
 import com.hyunmin.gpt.domain.chat.service.ChatGptService;
 import com.hyunmin.gpt.domain.chat.service.ChatService;
+import com.hyunmin.gpt.domain.message.service.MessageService;
 import com.hyunmin.gpt.global.security.annotation.AuthMember;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,11 +24,13 @@ public class ChatController {
 
     private final ChatService chatService;
     private final ChatGptService chatGptService;
+    private final MessageService messageService;
 
     @PostMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<Flux<String>> streamChat(@AuthMember Long memberId, @RequestBody @Valid ChatRequestDto requestDto) {
         String chatId = chatService.getOrCreateChatId(memberId, requestDto);
-        Flux<String> responseFlux = chatGptService.streamChat(chatId, requestDto);
+        ChatGptRequestDto gptRequestDto = messageService.readMessagesForChatGpt(chatId, requestDto.content());
+        Flux<String> responseFlux = chatGptService.streamChat(chatId, gptRequestDto, requestDto.content());
         return ResponseEntity.ok()
                 .header("X-Accel-Buffering", "no")
                 .body(responseFlux);
